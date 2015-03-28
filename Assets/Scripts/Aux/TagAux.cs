7b093c6bd345ba31fb25af009d64929358e0306b
Marc Guasch <marc.guasch@dharumastudio.com>
2015-03-28 11:16:19 +0100

    Enemigos power

--- Assets/Scripts/Aux/TagAux.cs
+++ Assets/Scripts/Aux/TagAux.cs
@@ -4,4 +4,5 @@
 	public const string PLAYER_BULLET_TAG = "PlayerBullet";
 	public const string PLAYER_TAG = "Player";
 	public const string PLAYER_IA_TAG = "IA";
+	public const string PLAYER_SHIELD_TAG = "Shield";
 }
